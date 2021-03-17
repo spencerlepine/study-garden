7e71286f1f9d06f27bd1c6acfa3ebf36cb6a03f7
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-03-17 10:02:41 -0400

    use local svg

--- client/src/components/samplePlants.js
+++ client/src/components/samplePlants.js
@@ -1,9 +1,9 @@
-//import Flower from "../images/flower.svg"
+import Oak from "../images/oak.svg"
 
 const samplePlants = [
     {id: 0, type: 'flower', 'min': 25, unlocked: true, source: 'https://www.pinclipart.com/picdir/big/550-5502647_pink-flower-clipart-png-download.png'},
     {id: 1, type: 'bush', 'min': 25, unlocked: true, source: 'https://opengameart.org/sites/default/files/bush_10.png'},
-    {id: 2, type: 'oak', 'min': 20, unlocked: true, source: 'https://dodo.ac/np/images/thumb/d/d9/Oak_Tree_NH.png/1200px-Oak_Tree_NH.png'},
+    {id: 2, type: 'oak', 'min': 20, unlocked: true, source: Oak},
     {id: 3, type: 'pine', 'min': 45, unlocked: true, source: 'https://i.pinimg.com/originals/d2/ba/c1/d2bac11943542656f60d7f21a437f057.png'},
     {id: 4, type: 'redwood', 'min': 60, unlocked: true, source: 'https://www.pngkey.com/png/full/13-131211_big-tree-clipart-tree-clipart-transparent.png'}
 ]
